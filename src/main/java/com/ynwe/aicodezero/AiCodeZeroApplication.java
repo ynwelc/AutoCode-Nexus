05b46930fa3a780f94cc6eb3c74143326ac13878
ynwelc <14992140+ynwelc@user.noreply.gitee.com>
2025-10-21 19:15:14 +0800

    项目初始化成功

---
+++ src/main/java/com/ynwe/aicodezero/AiCodeZeroApplication.java
@@ -0,0 +1,13 @@
+package com.ynwe.aicodezero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCodeZeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCodeZeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ynwe/aicodezero/AiCodeZeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynwe.aicodezero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeZeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
